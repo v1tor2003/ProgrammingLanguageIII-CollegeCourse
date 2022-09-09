d8ba0ed289957bd19bd90e2513c3e54521c50370
v1tor2003 <vitor.pr04@hotmail.com>
2022-09-09 00:12:32 -0300

    second commit

---
+++ BattleShip/Craps.java
@@ -0,0 +1,65 @@
+public class Craps{
+  public static void main(String[] args){
+    int cubes[] = new int[2];
+    readCubes(cubes);
+    firstTurn(cubes); 
+  }
+
+  private static void firstTurn(int cubes[]){
+    int sum = sumCubes(cubes);
+    checkCubesSum(sum);
+    secondTurn(cubes, sum);
+  }
+
+  private static int sumCubes(int cubes[]){
+    int result = 0;
+    for (int i = 0; i < cubes.length; i++){
+      result += cubes[i];
+    }
+    return result;
+  }
+
+  private static void checkCubesSum(int sum){
+    if(sum == 7 || sum == 11){
+      System.out.println("Vc ganhou :-)");
+      System.exit(0);
+    }else if (sum == 2 || sum == 3 || sum == 12){
+      System.out.println("Vc perdeu :-(");
+      System.exit(0);
+    }
+  }
+
+  private static void secondTurn(int cubes[], int point){    
+    System.out.println("Ponto: " + point);
+    System.out.println("Iniciando estagio 2...");
+    int turn = 0;
+    while(true){
+      readCubes(cubes);
+      turn = countTurns(turn);
+      int sum = sumCubes(cubes);
+      System.out.println("Jogada "+turn + " : " + sum);
+      if(sum == 7){
+        System.out.println("Vc perdeu :-(");
+        System.exit(0);
+      }else if (sum == point){
+        System.out.println("Vc ganhou :-)");
+        System.exit(0);
+      }
+    }
+  }
+
+  private static int countTurns(int turn){
+    return turn += 1;
+  }
+
+  private static void readCubes(int cubes[]){
+    final int min = 1;
+    final int max = 6;
+    for(int i = 0; i < cubes.length; i++){
+      cubes[i] = (int)Math.floor(Math.random()*(max-min+1)+min);
+    }
+    System.out.println("Dado 1: "+ cubes[0] + ", Dado 2: "+cubes[1]);
+  }
+
+  
+}
---
+++ BattleShip/Main.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[]args){
+    char player1Array[][] = new char[5][5];
+    char player2Array[][] = new char[5][5];
+    int size = player1Array.length;
+    initializePlayerArray(player1Array, size);
+    initializePlayerArray(player2Array, size);
+    
+    readShipsCoordinates(player1Array, size, 1);
+    showPlayerArray(player1Array, size);
+    readShipsCoordinates(player2Array, size, 2);
+    
+    //showPlayerTurn(true)
+    showPlayerArray(player2Array, size);
+  }
+
+  private static boolean validateShipsInput(int row, int column){
+    if((row >= 0 && row <= 4) && (column >= 0 && column <= 4)){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  private static void readShipsCoordinates(char playerArray[][], int size, int playerId){
+    System.out.println("PLAYER "+ playerId + " , ENTER  YOUR  SHIP'S COORDINATES.");
+    Scanner input = new Scanner(System.in);
+    int i = 0;
+    int row, column;
+
+    while (i < size){
+      System.out.println("Enter ship "+ (i+1)+ " location:");
+      row = input.nextInt();
+      column = input.nextInt();
+      System.out.println(row + " " + column);
+      
+      if(validateShipsInput(row, column)){
+        if(playerArray[row][column] != '@'){
+          playerArray[row][column] = '@';
+          i++;
+        }else{
+          System.out.println("You already have a ship there. Choose different coordinates.");
+        }
+      }else {
+        System.out.println("Invalid coordinates. Choose different coordinates.");
+      }
+    }
+  }
+
+  private static void initializePlayerArray(char playerArray[][], int size){
+    for(int i = 0; i < size; i++){
+      for(int j = 0; j < size; j++){
+        playerArray[i][j] = '-'; 
+      }
+    }
+  }
+
+  private static void showPlayerArray(char playerArray[][], int size){
+    int i = 0;
+    
+    System.out.println("  ");
+    for(i = 0; i < size; i++){
+      System.out.print(i + " ");
+    }
+    System.out.println("");
+    
+    for(i = 0; i < size; i++){
+      System.out.print(i + " ");
+      for(int j = 0; j < size; j++){
+        if(j == size - 1){
+          System.out.println(playerArray[i][j]); 
+        }
+        else{
+          System.out.print(playerArray[i][j] + " "); 
+        }
+      }
+    }
+    System.out.println("");
+  }
+}
