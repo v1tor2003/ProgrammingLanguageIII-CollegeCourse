7f758eaf46d8b1280db77d523538973891dddf5e
v1tor2003 <vitor.pr04@hotmail.com>
2022-11-25 12:11:31 -0300

    start-of-weight-units-lib

---
+++ List06/statics/UnidadesDePeso.java
@@ -0,0 +1,20 @@
+package List06.statics;
+
+public class UnidadesDePeso {
+  // gramasToKg()
+  // gramasToPounds()
+  // gramasToOunces()
+  
+  // kgToGramas()
+  // kgToPounds()
+  // kgToOunces()
+
+  // poundsToKg()
+  // poundsToGramas()
+  // poundsToOunces()
+
+  // ouncesToKg()
+  // ouncesToGramas()
+  // ouncesToPounds()
+
+}
